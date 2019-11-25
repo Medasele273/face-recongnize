455868d6bcfcdebfb46feacce2aa7ce813f69cbd
Medasele273 <medasele273@gmail.com>
2019-11-25 03:27:57 +0100

    Rank component is added and Modified

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const ImageLinkForm = ()=> {
+const ImageLinkForm = () => {
     return (
         <div>
             <p className="f3">
-                {'This Magic Brain will detect your face in your picture'}  
+                {'This Magic Brain will detect your face in your picture.Get it try'}
             </p>
             <div className="center">
                 <div className="center form br3 ba bw1 pa4 shadow-5 ">
-                    <input className="f3 pa1 w-70 " type="text"/>
-                    <button className="w-30 ma2 f3 br2 pa2 pv2 grow link dib bg-light-purple ">Detect</button>
+                    <input className="f3 pa1 w-70 " type="text" />
+                    <button className="w-30 ma1 f2 br2  pv2 grow link dib bg-light-purple ">Detect</button>
                 </div>
-                
+
             </div>
         </div>
     );
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rank = ()=>{
+    return (
+        <div>
+            <h2>{"Meda,Your current Rank is ... #5"}</h2>
+        </div>
+    );
+}
+
+export default Rank;
