3110ffc8b461ab2a9c9c6aec6099ed0c12a7583a
Medasele273 <medasele273@gmail.com>
2020-02-25 07:02:08 +0100

    Updates

---
+++ precache-manifest.79e97963208a39410ccd5e22f7d1aceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fb3efd1a5d7923d0dd0f0c686ae3cf",
+    "url": "/face-recongnize/index.html"
+  },
+  {
+    "revision": "0bc3c85d3e7279fc985a",
+    "url": "/face-recongnize/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6c82060ad8be41e86801",
+    "url": "/face-recongnize/static/css/main.741a4dcb.chunk.css"
+  },
+  {
+    "revision": "0bc3c85d3e7279fc985a",
+    "url": "/face-recongnize/static/js/2.bc86d0e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/face-recongnize/static/js/2.bc86d0e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c82060ad8be41e86801",
+    "url": "/face-recongnize/static/js/main.09d59674.chunk.js"
+  },
+  {
+    "revision": "3c4a8d97fe03574d47e1",
+    "url": "/face-recongnize/static/js/runtime-main.d13f2422.js"
+  }
+]);
